adb58156be971b22b4d76d9f1025a18ad65f8039
SergioCV06 <scastelov24@fpcoruna.afundacion.org>
2025-10-28 19:20:33 +0100

    Ejercicio1

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,16 @@
+def suma(a, b):
+    return a + b
+
+
+def resta(a, b):
+    return a - b
+
+
+def multiplicacion(a, b):
+    return a * b
+
+
+def division(a, b):
+    if b == 0:
+        return "Error: no se puede dividir entre 0"
+    return a / b
