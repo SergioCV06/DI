de94ce992fe4bbf7c8b83ced9beeb6df8f6cb708
SergioCV06 <scastelov24@fpcoruna.afundacion.org>
2025-11-10 17:09:30 +0100

    Ejercicio2

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,16 +1,21 @@
 package com.example.mycatalog;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
 
 public class CatalogActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_catalog);
+
+        Button boton = findViewById(R.id.buttonDetalle);
+        boton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, DetailActivity.class);
+            startActivity(intent);
+        });
     }
 }
